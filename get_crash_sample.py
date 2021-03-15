bf79f82a0aea40272dfd4d0531eb5cf456e189f4
mickstarego <mds8dp@virginia.edu>
2021-03-15 14:36:33 -0400

    smaller sample size

--- get_crash_sample.py
+++ get_crash_sample.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-pd.read_csv("crash_data_raw.csv").sample(frac=.05).to_csv("crash_data_sample.csv")
+pd.read_csv("crash_data_raw.csv").sample(frac=.05).to_csv("crash_data_sample.csv", index=False)
